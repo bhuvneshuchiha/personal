8dfe3c126fa31ef811f0e07a032c64e643784ab8
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-05-25 04:22:25 +0530

    react

---
+++ live_interview_platform/frontend/src/components/ChatWindow.jsx
@@ -0,0 +1,12 @@
+function ChatWindow() {
+	return (
+		<>
+			<h1> The Platform  </h1>
+			<label>
+				Candidate : <input type="myInput" placeholder="Try and answer the questions"/><br/>
+			</label>
+		</>
+	);
+}
+
+export default ChatWindow
