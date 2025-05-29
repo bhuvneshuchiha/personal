be9fdad0f81a219de261b12ec3b61d4db7dd2a30
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-05-29 23:41:26 +0530

    Stop tracking project_mordoria to avoid conflicts; treat it as separate repo

--- project_mordoria/README.md
+++
@@ -1,3 +0,0 @@
-#
-1. The messages coming from backend are being stored in an array in the js file.
-2. Just use something like useState to render that array on to the screen
--- project_mordoria/pkg/ai_interceptor/ai_message.go
+++
@@ -1,9 +0,0 @@
-package ai_interceptor
-
-import "github.com/bhuvneshuhciha/project_mordoria/internal/message"
-
-type IncomingMessages struct {
-	ClientId string            `json:"client_id"`
-	Payload  []message.Message `json:"payload"`
-	// AiEmotScore string            `json:"ai_emot_score"`
-}
