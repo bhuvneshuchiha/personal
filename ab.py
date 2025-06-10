f956b8a583160252399316333486816adc74bdba
Bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-06-10 12:14:37 +0530

    document_separator

--- ab.py
+++ ab.py
@@ -1,14 +1,25 @@
-# arr = "abc"
-arr = "xyz"
-strlst = list(arr)
-lst = []
+from PyPDF2 import PdfWriter, PdfReader
 
-for i in range(len(arr)-1):
-    for j in range(i, len(arr)-1):
-        temp = ""
-        temp = strlst[i]
-        strlst[i] = strlst[j]
-        strlst[j] = temp
-    lst.append("".join(strlst))
+def split_pdf_to_two(filename,page_number):
+    pdf_reader = PdfReader(open(filename, "rb"))
+    try:
+        assert page_number < len(pdf_reader.pages)
+        pdf_writer1 = PdfWriter()
+        pdf_writer2 = PdfWriter()
 
-print(lst)
+        for page in range(page_number):
+            pdf_writer1.add_page(pdf_reader.pages[page])
+
+        for page in range(page_number,len(pdf_reader.pages)):
+            pdf_writer2.add_page(pdf_reader.pages[page])
+
+        with open("part1.pdf", 'wb') as file1:
+            pdf_writer1.write(file1)
+
+        with open("part2.pdf", 'wb') as file2:
+            pdf_writer2.write(file2)
+
+    except AssertionError as e:
+        print("Error: The PDF you are cutting has less pages than you want to cut!")
+
+split_pdf_to_two("epod.pdf", 1)
